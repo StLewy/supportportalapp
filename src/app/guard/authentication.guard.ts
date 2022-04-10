661aceb906621a83033888d54d13ff38f163c3db
slewandowski <st.lewy@gmail.com>
2022-04-10 21:52:07 +0200

    adding notification

--- src/app/guard/authentication.guard.ts
+++ src/app/guard/authentication.guard.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {AuthenticationService} from "../service/authentication.service";
+import {NotificationService} from "../service/notification.service";
+import {NotificationType} from "../enum/notification-type.enum";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
 
-  constructor(private authenticationService: AuthenticationService, private router: Router) {
+  constructor(private authenticationService: AuthenticationService, private router: Router, private notificationService: NotificationService ) {
   }
   canActivate(
     next: ActivatedRouteSnapshot,
@@ -20,7 +22,7 @@ export class AuthenticationGuard implements CanActivate {
       return true
     }
     this.router.navigate(['/login']);
-    // TODO - send notification to user
+    this.notificationService.notify(NotificationType.ERROR, `You need to log in to access this page`.toUpperCase());
     return false;
   }
 
